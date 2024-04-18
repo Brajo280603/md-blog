e5be7361a40cf5c0938cee582473885c165b1884
Brajananda Mahata <75053676+Brajo280603@users.noreply.github.com>
2024-04-18 18:27:59 +0530

    Update +page.svelte

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -7,4 +7,4 @@
     A Markdown Blog
 </h1>
 <hr>
-<p>This is a site, <br> in which you can blog stuff by writing them in markdown format and committing them to this github repo</p>
+<p>This is a site, <br> in which you can blog stuff by writing them in markdown format and committing them to <span><a href="https://github.com/Brajo280603/md-blog">this github repo</a></span></p>
