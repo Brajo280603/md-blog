526e4c6d2021521269be1632910bccb8f170554d
Brajananda Mahata <brajanandamahato280603@gmail.com>
2024-04-18 18:01:29 +0530

    fix link

--- src/lib/config.js
+++ src/lib/config.js
@@ -2,4 +2,4 @@ import { dev } from "$app/environment"
 
 export const title = "Blogs"
 export const description = "SvelteKit Blog for Developers and Poets Alike"
-export const url = dev ? 'http://localhost:5173' : 'https://dev.blog'
+export const url = dev ? 'http://localhost:5173' : 'https://md-blog-ten.vercel.app/'
